3cd921d4660dd5a606f1cd4a816f63ca032e4d0a
Changmin Lim <chang.lim92@gmail.com>
2018-05-25 09:10:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(int)
+  return false if int <= 1 
   sqr = (int ** 0.5).to_i 
   range = (2..sqr).to_a 
-  return false if int <= 1 
   range.each do |i|
     if int % i == 0 
       return false 

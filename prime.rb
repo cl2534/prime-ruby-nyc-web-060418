f324c5cae85282a00934d75cf6175ec8555d3b33
Changmin Lim <chang.lim92@gmail.com>
2018-05-25 09:07:12 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(int)
   range = (2..int-1).to_a 
-  return false if int < 2 
+  return false if int <= 1 
   range.each do |i|
     if num % i == 0 
       return false 
@@ -9,11 +9,3 @@ def prime?(int)
   end 
   return true
 end 
-  
-  for i in new_array do
-+		if num % i == 0
-+			return false
-+		end	
-+	end
-+	return true
-+end 
